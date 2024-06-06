13197327e01f9ecf31a9ede666433ffaa22e6c22
Aashik Shihaab <sihaabama@gmail.com>
2024-06-07 02:54:37 +0530

    final

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/v1", routes);
 
+// Start server
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
